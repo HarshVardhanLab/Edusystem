9b30f0c8627eadd7ac17e4cce030bd8fa980d282
harshVardhanLab <harshvardhan02102002@gmail.com>
2026-01-31 20:03:01 +0530

    Fix: Remove file logging for Render deployment

--- library_backend/settings.py
+++ library_backend/settings.py
@@ -171,20 +171,14 @@ LOGGING = {
         },
     },
     'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
-            'formatter': 'verbose',
-        },
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
         },
     },
     'root': {
-        'handlers': ['console', 'file'],
+        'handlers': ['console'],
         'level': 'INFO',
     },
 }
